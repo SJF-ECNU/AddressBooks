570f9db79334adc02a5de220b85b99e07d65efe6
SJF-ECNU <10225501403@stu.ecnu.edu.cn>
2023-08-09 11:04:05 +0800

    添加项目文件。

---
+++ 通讯录管理/通讯录管理.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+//展示菜单
+void showMenu()
+{
+	cout << "1.添加联系人\n2.显示联系人\n3.删除联系人\n4.查找联系人\n5.修改联系人\n6.清空联系人\n0.退出通讯录" << endl;
+
+}
+
+int main()
+{
+	int select = 0;
+
+	showMenu();
+
+	cin >> select;
+
+	switch (select)
+	{
+	case 0://退出通讯录
+		cout << "欢迎下次使用通讯录系统，按任意键退出" << endl;
+		break;
+	case 1://添加联系人
+		break;
+	case 2://显示联系人
+		break;
+	case 3://删除联系人
+		break;
+	case 4://查找联系人
+		break;
+	case 5://修改联系人
+		break;
+	case 6://清空联系人
+		break;
+	default:
+		break;
+	}
+
+	system("pause");
+	return 0;
+}
